894ff7e7f1c8df6c7b1b21f54287e67b87472e3a
mahdi saeikhalili <mahdi.saeikhalili@gmail.com>
2017-02-19 21:47:47 -0500

    add code to script file to get the ajax work

--- myScript.js
+++ myScript.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+function getRandomQuote(){
+	
+	$.ajax({
+		url: "http://api.forismatic.com/api/1.0/",
+		dataType: "jsonp",
+		data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+		success: function(response){
+			alert(response.quoteText);
 
+		}
+	});
+
+}
+
+$(document).ready(function(){
+	getRandomQuote();
 });
